00f13b3d824898e78377005bcabb510fbec7486e
VANNNTSE61626\VanNNT <vannnt2112@gmail.com>
2017-09-11 13:41:06 +0700

    valid register

--- capstone-client/src/app/app.component.ts
+++ capstone-client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as $ from 'jquery';
 import {AuthService} from 'angular2-social-login';
 import {LoginService} from './service/login/login.service';
+import {NgForm} from "@angular/forms";
+import { CompleterService, CompleterData } from 'ng2-completer';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,22 @@ import {LoginService} from './service/login/login.service';
 export class AppComponent implements OnInit, OnDestroy {
   public user;
   sub: any;
-  constructor(private auth: AuthService, private loginService: LoginService) {}
+  protected searchStr: string;
+  protected captain: string;
+  protected dataService: CompleterData;
+  protected searchData = [
+    { color: 'red', value: '#f00' },
+    { color: 'green', value: '#0f0' },
+    { color: 'blue', value: '#00f' },
+    { color: 'cyan', value: '#0ff' },
+    { color: 'magenta', value: '#f0f' },
+    { color: 'yellow', value: '#ff0' },
+    { color: 'black', value: '#000' }
+  ];
+  protected captains = ['James T. Kirk', 'Benjamin Sisko', 'Jean-Luc Picard', 'Spock', 'Jonathan Archer', 'Hikaru Sulu', 'Christopher Pike', 'Rachel Garrett' ];
+  constructor(private auth: AuthService, private loginService: LoginService, private completerService: CompleterService) {
+    this.dataService = completerService.local(this.searchData, 'color', 'color');
+  }
   ngOnInit() {
     $(window).scroll(function () {
     const height = $(window).scrollTop();
@@ -45,7 +62,10 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     );
   }
-
+  onSubmit(registerForm : NgForm){
+    console.log(registerForm.value);
+    registerForm.reset();
+  }
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
--- capstone-client/src/app/app.module.ts
+++ capstone-client/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import {appRoutes} from './app.routes';
-
+import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 // Component
 import { AppComponent } from './app.component';
 import { CompanyDetailComponent } from './component/company-detail/company-detail.component';
 import { Angular2SocialLoginModule } from 'angular2-social-login';
 import { ReviewRatingComponent } from './component/review-rating/review-rating.component';
-
+import { Ng2CompleterModule } from "ng2-completer";
 // Service
 import {LoginService} from './service/login/login.service';
 import {BaseService} from './service/base-service/base.service';
@@ -41,7 +42,11 @@ const providers = {
     ReactiveFormsModule,
     HttpModule,
     MaterialModule,
-    Angular2SocialLoginModule
+    Angular2SocialLoginModule,
+    BrowserAnimationsModule,
+    Ng2AutoCompleteModule,
+    Ng2CompleterModule,
+
   ],
   providers: [LoginService, BaseService, CheckLoginGuard],
   bootstrap: [AppComponent]
