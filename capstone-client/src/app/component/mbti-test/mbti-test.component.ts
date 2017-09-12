5b602ad5b0edf9b3ba44492bdd0fd93295dba87d
VANNNTSE61626\VanNNT <vannnt2112@gmail.com>
2017-09-12 11:34:38 +0700

    sửa một số thứ trong trang mbti-test

--- capstone-client/src/app/component/mbti-test/mbti-test.component.ts
+++ capstone-client/src/app/component/mbti-test/mbti-test.component.ts
@@ -9,8 +9,31 @@ import {Router} from '@angular/router';
 export class MbtiTestComponent implements OnInit {
 
   constructor(private router: Router) { }
-
+  questions=[
+    {
+      id : '1',
+      question: 'Bạn Có Lên Danh Sách Cho Các Việc Cần Làm Không?'
+    },
+    {
+      id : '2',
+      question: 'Bạn Có Lên Danh Sách Cho Các Việc Cần Làm Không?'
+    },
+    {
+      id : '3',
+      question: 'Bạn Có Lên Danh Sách Cho Các Việc Cần Làm Không?'
+    },
+    {
+      id : '4',
+      question: 'Bạn Có Lên Danh Sách Cho Các Việc Cần Làm Không?'
+    },
+    {
+      id : '5',
+      question: 'Bạn Có Lên Danh Sách Cho Các Việc Cần Làm Không?'
+    },
+  ];
   ngOnInit() {
   }
-
+  onSubmit(value){
+    console.log(value);
+  }
 }
