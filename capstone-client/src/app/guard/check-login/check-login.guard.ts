709ea1f631b253342f9754191312309367ed3c8f
VANNNTSE61626\VanNNT <vannnt2112@gmail.com>
2017-09-15 09:59:32 +0700

    chuyển hướng page về home khi chua login

--- capstone-client/src/app/guard/check-login/check-login.guard.ts
+++ capstone-client/src/app/guard/check-login/check-login.guard.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate} from '@angular/router';
+import {CanActivate, Router} from '@angular/router';
 import {LoginService} from '../../service/login/login.service';
 import {BaseService} from '../../service/base-service/base.service';
 
 @Injectable()
 export class CheckLoginGuard implements CanActivate {
-  constructor(private loginService: LoginService, private baseService: BaseService) {}
+  constructor(private loginService: LoginService, private baseService: BaseService,private router: Router) {}
   canActivate() {
     const status = this.loginService.checkLogged();
     if (!status) {
        this.baseService.showLoginForm();
+      this.router.navigate(['']);
     }
     return status;
   }
