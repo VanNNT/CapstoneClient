5909d097e2651e3ad8a49fda069021b08571fd27
VANNNT <vannnt2112@gmail.com>
2017-10-29 10:17:56 +0700

    no message

--- capstone-client/src/app/app.module.ts
+++ capstone-client/src/app/app.module.ts
@@ -51,8 +51,6 @@ import { OrderByPipe, GroupByPipe } from './pipes/order-by.pipe';
 import { EditMajorComponent } from './component/admin/edit-major/edit-major.component';
 import {DecimalPipe} from "@angular/common";
 import { MajorDetailComponent } from './component/major-detail/major-detail.component';
-
-import {MatTooltipModule} from '@angular/material'
 const providers = {
   'google': {
     'clientId': '525175435249-fcon5618ugd00jij2o066g7k3266n3mb.apps.googleusercontent.com'
